fcc5eb85ded145ee6f9c20ac0a90fb3825006de4
Robbie Ostrow <ostrowr@gmail.com>
2021-01-17 17:32:22 -0800

    add link to source

--- src/title.tsx
+++ src/title.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
+import { Github } from "react-bootstrap-icons";
 
 import { Rules } from "./rules";
 
@@ -18,6 +19,15 @@ export const Title: React.FC = (props) => {
         >
           How to Play
         </Button>
+        <a
+          href="https://github.com/ostrowr/permiscuchess"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button variant="outline-secondary" size="sm">
+            <Github /> View source
+          </Button>
+        </a>
       </Jumbotron>
       <Rules show={showRules} onHide={() => setShowRules(false)} />
     </>
